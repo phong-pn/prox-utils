117b4b636238f9b23e6fed27ae80f946a7c80686
rolandonogoldgoal <68616009+rolandonogoldgoal@users.noreply.github.com>
2022-09-08 16:12:43 +0700

    add countdown

--- app/src/main/java/com/proxglobal/lib/testsale/TestSaleOffActivity.kt
+++ app/src/main/java/com/proxglobal/lib/testsale/TestSaleOffActivity.kt
@@ -1,15 +1,12 @@
 package com.proxglobal.lib.testsale
 
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.google.firebase.ktx.Firebase
-import com.phongpn.countdown.countdownview.CountDownConfig
-import com.proxglobal.countdownview.countdownview.CountdownView
+import com.phongpn.countdown.countdown.CountDownView
+import com.phongpn.countdown.util.dp
 import com.proxglobal.lib.MainActivity3
 import com.proxglobal.lib.R
 import com.proxglobal.sale.controller.ConditionOnShowSaleBehavior
@@ -17,13 +14,13 @@ import com.proxglobal.sale.controller.ProxSale
 import com.proxglobal.sale.controller.behavior.DefaultShowBannerSaleBehavior
 import com.proxglobal.sale.model.sale.SaleEvent
 import com.proxglobal.sale.model.sale.SaleScript
-import com.proxglobal.sale.utils.dp
 
 
 class TestSaleOffActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test_sale_off)
@@ -39,22 +36,25 @@ class TestSaleOffActivity : AppCompatActivity() {
             })
         }
         findViewById<View>(R.id.bt_pause).setOnClickListener {
-            ProxSale.showBanner(findViewById(R.id.banner_container), 3, object: DefaultShowBannerSaleBehavior(findViewById(R.id.banner_container)) {
-                override fun onShow(event: SaleEvent, script: SaleScript) {
-                    findViewById<CountdownView>(R.id.count_down).apply {
-                        dynamicShow(CountDownConfig(
-                            suffixTextSize = 16f,
-                        ))
-                    }.start(30000)
-                    findViewById<View>(R.id.banner).isVisible = true
-                    super.onShow(event, script)
-                }
+            ProxSale.showBanner(
+                findViewById(R.id.banner_container),
+                3,
+                object : DefaultShowBannerSaleBehavior(findViewById(R.id.banner_container)) {
+                    override fun onShow(event: SaleEvent, script: SaleScript) {
+                        findViewById<CountDownView>(R.id.count_down)
+                            .modifier {
+                                timePaddingLeft = 2.dp
+                                timePaddingRight = 2.dp
+                            }
+                        findViewById<View>(R.id.banner).isVisible = true
+                        super.onShow(event, script)
+                    }
 
-                override fun onCancel(event: SaleEvent, script: SaleScript) {
-                    super.onCancel(event, script)
-                    findViewById<View>(R.id.banner).isVisible = false
-                }
-            })
+                    override fun onCancel(event: SaleEvent, script: SaleScript) {
+                        super.onCancel(event, script)
+                        findViewById<View>(R.id.banner).isVisible = false
+                    }
+                })
 
         }
         findViewById<View>(R.id.bt_next).setOnClickListener {
@@ -71,17 +71,5 @@ class TestSaleOffActivity : AppCompatActivity() {
                     startActivity(Intent(this, MainActivity3::class.java))
             }
         )
-//        findViewById<CountdownView>(R.id.count_down).apply {
-//            dynamicShow(DynamicConfig.Builder()
-//                .setBackgroundInfo(
-//                    DynamicConfig.BackgroundInfo()
-//                        .setColor(Color.parseColor("#FFCC0000"))
-//                        .setSize(60.dp)
-//                        .setRadius(2.dp)
-//                        .setShowTimeBgDivisionLine(false)
-//                )
-//                .build())
-//            start(3600000)
-//        }
     }
 }
